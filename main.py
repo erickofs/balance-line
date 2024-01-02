f3495f4c3d521633fdccba14c567b63d87a9afd9
Erick Fernandes <80919235+erickofs@users.noreply.github.com>
2024-01-02 18:35:26 -0300

    Update main.py
    Update main to add docstring and change file handler

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 # Balance line:
 
 # 1. Ler dois arquivos (file1.txt e file2.txt) que estão em ordem alfabética linha por linha.
-# 2. Gravar um terceiro arquivo (file3.txt) contendo a soma dos dois arquivos de entrada, mantendo a ordem alfabética.
-# 3. Ler linha por linha, não ler nenhum dos dois arquivos todos para a memória, pois podem ser arquivos gigantescos.
+# 2. Gravar um arquivo (file3.txt) com a soma dos dois arquivos de entrada em ordem alfabética.
+# 3. Ler linha por linha, sem jogar para a memória, pois podem ser arquivos gigantescos.
 
+"""
+This program: 
+1. reads two files (file1.txt and file2.txt) that are in alphabetical order line by line 
+2. writes a third file (file3.txt) in alphabetical order containing the sum of the two input files.
+"""
 # Abrir  arquivos 1 e 2 read
-f1 = open('file1.txt')
-f2 = open('file2.txt')
+f1 = open('file1.txt', 'r', encoding='utf-8')
+f2 = open('file2.txt', 'r', encoding='utf-8')
 # Abrir arquivo 3 write
-f3 = open('file3.txt', 'w')
+f3 = open('file3.txt', 'w', encoding='utf-8')
 # Ler a primeira linha do arquivo 1 (linha1)
 linha1 = f1.readline().strip()
 # Ler a primeira linha do arquivo 2 (linha2)
